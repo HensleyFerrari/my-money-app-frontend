ed16235df358427e53a205b6342ca2902bfb36f0
HensleyFerrari <hensley_ferrari@hotmail.com>
2021-10-05 21:39:01 -0300

    initial commit

---
+++ src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+    <footer className="main-footer">
+        <strong>
+            Copyright &copy; 2021
+            <a href="https://github.com/HensleyFerrari" target='_blank'> Hensley</a>
+        </strong>
+    </footer>
+)
